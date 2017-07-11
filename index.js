86a70c9f225b0d952ca60ac3ba1f199a2b80bb49
tschaumburg <thomas@schaumburg-it.dk>
2017-07-11 11:10:30 +0200

    0.0.1

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-﻿var conf = require("../shared-config");
+﻿var path = require('path');
+var findParentDir = require('find-parent-dir');
 
-module.exports = conf;
+function findSharedConfig(filename)
+{
+    console.log("Starting in " + __dirname);
+
+    var dir = findParentDir.sync(__dirname, filename);
+    console.log("dir:  " + dir);
+
+    var file = path.join(dir, filename);
+    console.log("file: " + file);
+
+    if (!!dir)
+    {
+        var conf = require(file);
+        console.log("conf: " + JSON.stringify(conf));
+        return conf;
+    }
+}
+
+module.exports = findSharedConfig;//('shared-config.js');
 
